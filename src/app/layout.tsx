816348be8f03233081122921cf1f47b70e4e03cd
Vishnu Kulkarni <vishnu@starapps.studio>
2023-06-24 16:54:00 +0530

    sub reddit created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/Toaster";
+import Providers from "@/components/Provider";
 
 export const metadata = {
   title: "soco",
@@ -25,13 +26,15 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-6 bg-slate-50 antialiased">
-        {/* @ts-expect-error Server Component */}
-        <Navbar />
-        {authModal}
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
-        <Toaster />
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          {authModal}
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
