ec7705b11256499858c952e8d52b0c4ac478e6e3
Vishnu Kulkarni <vishnu@starapps.studio>
2023-06-25 14:57:07 +0530

    minor change to import

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -1,6 +1,6 @@
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
-import { SubredditValidator } from '@/lib/validators /subreddit'
+import { SubredditValidator } from '@/lib/validators/subreddit'
 
 import { z } from 'zod'
 
